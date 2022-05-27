50452a9c65808cf8b7c6ce821928dda51a0e32ec
Joel Jaldin <jsejaldin@gmail.com>
2022-05-26 22:52:29 -0300

    finished

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,29 @@
+class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new(review_params)
+    @review.restaurant = @restaurant
+
+    # antes de que se guarde en la linea siguiente, la variable @review quedará con los siguientes datos.
+    # en teoria lo que estamos haciendo: @review.restaurant_id = 1, @review.content = "muy buen restaurant", @review.rating = 5
+    # @review = Review.new(restaurant_id: 1, content: "muy buen restaurant", rating: 5)
+
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants
+  Rails.application.routes.draw do
+    resources :restaurants do
+      resources :reviews, only: [ :index, :new, :create ]
+    end
+    resources :reviews, only: [ :show, :edit, :update, :destroy ]
+  end
 end
+
+# restaurants/:id/reviews/:id
+# restaurants/restaurant_id/reviews/id
+#  = Restaurant.find(params[:restaurant_id])
